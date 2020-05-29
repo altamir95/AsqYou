0803bd3efaf908fd5ef622954d235bd74eb2489b
altamir95 <altamir95@yandex.ru>
2020-05-29 22:21:02 +0300

    StartTwo

---
+++ Asqyou/Models/Asq.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AskDispaly.Models
+{
+    public class Asq
+    {
+        public int id { get; set; }
+        public string asq { get; set; }
+    }
+}
+
